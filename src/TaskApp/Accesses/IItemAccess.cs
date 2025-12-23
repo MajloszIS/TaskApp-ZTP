2be510d694bb28bb51fa8bbf4d653ca32ec44106
Majlosz <psp13msz@gmail.com>
2025-12-23 23:42:39 +0100

    authservice, access

--- src/TaskApp/Accesses/IItemAccess.cs
+++ src/TaskApp/Accesses/IItemAccess.cs
@@ -1,18 +1,13 @@
 using System;
 using System.Collections.Generic;
-using TaskApp.Commands;
 using TaskApp.Items;
-using TaskApp.Observer;
-using TaskApp.Access;
-using TaskApp.Repository;
-//niektóre trzeba usunąc, nie wszystkie są potrzebne
 
 namespace TaskApp.Access;
 
 public interface IItemAccess
 {
-    IItem GetItem(User user, Guid id);
-    List<IItem> GetItemsForUser(User user);
-    void SaveItem(User user, IItem item);
-    void ShareItem(User owner, User target, IItem item);
+    IItem GetItem(Guid userId, Guid itemId);
+    List<IItem> GetItemsForUser(Guid userId);
+    void SaveItem(Guid userId, IItem item);
+    void ShareItem(Guid ownerId, Guid targetId, Guid itemId);
 }
