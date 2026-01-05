cec9cbd04611cb150f5b6e1549825822397ef869
Yoshi6463 <92087@student.pb.edu.pl>
2026-01-05 23:57:13 +0100

    Command

--- src/TaskApp/Commands/DeleteItemCommand.cs
+++ src/TaskApp/Commands/DeleteItemCommand.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using TaskApp.Items;
 using TaskApp.Observer;
 using TaskApp.Access;
--- src/TaskApp/Commands/ICommand.cs
+++ src/TaskApp/Commands/ICommand.cs
@@ -1,12 +1,3 @@
-using System;
-using System.Collections.Generic;
-using TaskApp.Commands;
-using TaskApp.Items;
-using TaskApp.Observer;
-using TaskApp.Access;
-using TaskApp.Repository;
-//niektóre trzeba usunąc, nie wszystkie są potrzebne
-
 namespace TaskApp.Commands;
 
 public interface ICommand
