e1314fb70fce8473173e34e454342047f5ac7765
Yoshi6463 <92087@student.pb.edu.pl>
2026-01-12 21:49:39 +0100

    Usunięty auto backup

--- src/TaskApp/Commands/EditItemCommand.cs
+++ src/TaskApp/Commands/EditItemCommand.cs
@@ -8,6 +8,8 @@ public class EditItemCommand : ItemCommandBase
 {
     private string newTitle;
     private string newContent;
+    private string oldTitle;
+    private string? oldContent;
 
     public EditItemCommand(ItemManager itemManager, IItem item, string title, string content)
         : base(itemManager, item)
@@ -18,41 +20,30 @@ public class EditItemCommand : ItemCommandBase
 
     public override void Execute()
     {
-        CreateBackup();
+        oldTitle = item.Title;
 
-        item.Title = newTitle;
         if (item is Note note)
         {
-            note.Content = newContent;
+            oldContent = note.Content;
         }
-        
+
+        item.Title = newTitle;
+
+        if (item is Note n)
+        {
+            n.Content = newContent;
+        }
         itemManager.UpdateItem(item);
     }
 
     public override void Undo()
     {
-        if (backup == null) return;
+        item.Title = oldTitle;
 
-        var oldState = backup.StateSnapshot;
-
-        item.Title = oldState.Title;
-
-        if (item is Note liveNote && oldState is Note snapshotNote)
+        if (item is Note note && oldContent != null)
         {
-            liveNote.Content = snapshotNote.Content;
-            
-            if (snapshotNote.Tags != null)
-                liveNote.Tags = new List<string>(snapshotNote.Tags);
-            else
-                liveNote.Tags = new List<string>();
+            note.Content = oldContent;
         }
-        else if (item is Tasky liveTask && oldState is Tasky snapshotTask)
-        {
-            liveTask.DueDate = snapshotTask.DueDate;
-            liveTask.Priority = snapshotTask.Priority;
-            liveTask.IsCompleted = snapshotTask.IsCompleted;
-        }
-
         itemManager.UpdateItem(item);
     }
 }
